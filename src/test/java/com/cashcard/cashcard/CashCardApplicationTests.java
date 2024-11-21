14924cf741e3cae3595576f62cd6a80913591035
Brian Guterl <brianguterl99@gmail.com>
2024-11-20 23:44:05 -0500

    refactored tests for lists

--- src/test/java/com/cashcard/cashcard/CashCardApplicationTests.java
+++ src/test/java/com/cashcard/cashcard/CashCardApplicationTests.java
@@ -4,18 +4,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.net.URI;
 
 
 @SpringBootTest(webEnvironment =
 		SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class CashCardApplicationTests {
 	@Autowired
 	TestRestTemplate restTemplate;
@@ -47,6 +50,7 @@ class CashCardApplicationTests {
 	}
 
 	@Test
+	//@DirtiesContext
 	void shouldCreateANewCashCard() {
 		CashCard newCashCard = new CashCard(null, 250.00);
 		ResponseEntity<Void> createResponse =
